b0743b6fcc3f3b5df9072f7e4fb9d80945b7b528
olaide <pelumibinuyo@gmail.com>
2022-12-14 13:05:36 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "Programming is like building a multilingual
+ *  puzzle, followed by a newline."
+ *  return 0.
+ */
+
+int main (void)
+
+{
+
+	puts("\"programming is like building a multilingual puzzle");
+
+	return(0);
+
+
+
+}
