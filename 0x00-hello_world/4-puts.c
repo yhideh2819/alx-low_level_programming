86425a5ddadb429e548c128dbd94a568415d4921
olaide <pelumibinuyo@gmail.com>
2023-01-01 14:36:49 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual
+ * main - prints Programming is like building a multilingual
  *  puzzle, followed by a newline.
  *  Return: Always 0.
  */
@@ -13,4 +13,6 @@ int main(void)
 	puts("\"programming is like building a multilingual puzzle");
 
 	return (0);
+
+
 }	
