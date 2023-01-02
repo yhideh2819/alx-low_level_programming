32c8f433133197568503b2fd4384da8a9a770b9a
olaide <pelumibinuyo@gmail.com>
2023-01-02 11:16:58 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 
-	fput("and that piece of art is useful\" -Dora kopar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" -Dora kopar, 2015-10-19\n", stdout);
 	return (1);
 
 }
