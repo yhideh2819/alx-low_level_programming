04f4d58ffe7349613c8b5aa98222506b5c9eb258
olaide <pelumibinuyo@gmail.com>
2023-01-02 09:01:58 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main -print a line of code using puts
- *
+ * 
  *  Return: Always 0.
  */
 
@@ -10,6 +10,6 @@ int main(void)
 
 {
 
-	puts("\"Programming is like a multilingual puzzle");
+	puts("\"Programming is like multilingual puzzle");
 	return (0);
 }
