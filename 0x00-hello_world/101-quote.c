cb2368812926707b96062ec7d299697c7a145bd9
olaide <pelumibinuyo@gmail.com>
2023-01-03 13:48:28 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <unistd.h>
 
+
 /**
- * main - A c program that prints a line to the standard eror
- * Return: 1 (success)
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a newline, to the standard error.
+ * Return: Alway 1.
  */
+
 int main(void)
+
 {
-	write(2, "and that piece of art is useful\" -Dora korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora kopar, 2015-10-19\n", 59);
 	return (1);
 }
