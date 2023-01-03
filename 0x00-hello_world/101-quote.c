cf8176f9154dce8cec4eb09583aee176975e9566
olaide <pelumibinuyo@gmail.com>
2023-01-03 13:36:06 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -7,8 +6,6 @@
  */
 int main(void)
 {
-
-	fputs("and that piece of art is useful\" -Dora korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" -Dora korpar, 2015-10-19\n",59);
 	return (1);
-
 }
