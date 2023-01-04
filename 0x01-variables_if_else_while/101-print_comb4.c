8f459aac7ebda624e4f375c40f3f876b11598723
olaide <pelumibinuyo@gmail.com>
2023-01-03 16:00:56 -0800

    print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,68 +1,38 @@
-
-
 #include <stdio.h>
-
-
+#include <stdlib.h>
+#include <time.h>
 
 /**
- *
- *  * main - prints all possible different combinations of three digits
- *
- *   * Return: Always 0 (Success)
- *
- *    */
+ * main - Print all possible combination for three digit
+ * Return: ALways 0 (Success)
+ */
 
 int main(void)
 
 {
+	int d, p, q;
 
-		int n, m, l;
+	for (d = '0'; d < '9'; d++)
+	{
 
+	for (p = d + 1; p <= '9'; p++)
+	{
 
-
-			for (n = 48; n < 58; n++)
-
-					{
-
-								for (m = 49; m < 58; m++)
-
-											{
-
-															for (l = 50; l < 58; l++)
-
-																			{
-
-																								if (l > m && m > n)
-
-																													{
-
-																																			putchar(n);
-
-																																								putchar(m);
-
-																																													putchar(l);
-
-																																																		if (n != 55 || m != 56)
-
-																																																								{
-
-																																																															putchar(',');
-
-																																																																					putchar(' ');
-
-																																																																										}
-
-																																																						}
-
-																											}
-
-																	}
-
-									}
-
-				putchar('\n');
-
-					return (0);
-
+	for (q = p + 1; q <= '9'; q++)
+	{
+	if ((p != q) != q)
+	{
+	putchar(d);
+	putchar(p);
+	putchar(q);
+	if (d == '7' && p == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
 }
-
