89e06e27e0277d518f1ff37ab1f828381ed37cb1
olaide <pelumibinuyo@gmail.com>
2023-01-04 06:41:40 -0800

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,18 +4,9 @@
 
 #include <stdio.h>
 
-
-
 void positive_or_negative(int i);
-
 int largest_number(int a, int b, int c);
-
+int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-int convert_day(int month, int day);
-
-
-
-#endif
-
-:
+#endif /* MAIN_H */
