a52a319204fd9c360c4fe9c1cb45fd8f280ac8b7
olaide <pelumibinuyo@gmail.com>
2023-01-05 13:48:50 -0800

    0-puts_recursion.c

---
+++ 0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts_recursion - function
+ * @s: parameter
+ * Return: out put
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_putchar_recursion(s + 1);
+
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
+
